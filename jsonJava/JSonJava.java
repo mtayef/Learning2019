ab772595c81d2f2433f20acf680d010fae66f16b
mtayef <30615194+mtayef@users.noreply.github.com>
2019-05-03 13:39:35 -0400

    Update JSonJava.java

--- jsonJava/JSonJava.java
+++ jsonJava/JSonJava.java
@@ -1,14 +1,20 @@
+package jsonJava;
+import com.fasterxml.jackson.databind;
+
 public Class JSonJava{
 	public static void main(String args[]){
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = null;
+		
+		CustomerDetails c = new CustomerDetails();
+		
 		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Business", "root","Internal101%");
 
 		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery("select * from CustomerInfo where location = 'Asia' and purchasedDate = curdate() Limit 1;");
-		while(rs.next){
-			CustomerDetails c = new CustomerDetails();
-			
+		ResultSet rs = st.executeQuery("select * from CustomerInfo where location = 'Asia' and purchasedDate = curdate();");
+		
+		
+		while(rs.next){			
 			//setters
 			c.setCourseName(rs.getString(1));
 			c.setPurchasedDate(rs.getString(2));
@@ -19,5 +25,8 @@ public Class JSonJava{
 			System.out.println(c.getCourseName());
 			System.out.println(c.getPurchasedDate());
 		}
+		ObjectMapper o = new ObjectMapper();
+		o.writeValue(new File(""),c);
+		con.close();
 	}
 }
